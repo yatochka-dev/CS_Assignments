3c05ac41c1577c8296925fc93452b742d824a215
saaasuuukaae <philipchef13@gmail.com>
2024-02-06 17:36:32 +0200

    Shitty task

---
+++ src/LockCodeChecker.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Scanner;
+
+public class LockCodeChecker {
+
+    public static void main(String[] args) {
+        //        System.out.print("Enter a 4-digit number (1000-9999): ");
+        //        int userNumber = inputNumber();
+        // run through 1000-9999
+
+        int maxDiff = 0;
+        int maxDiffNum = 0;
+        for (int i = 1000; i < 10000; i++) {
+
+            if (run(i)) {
+                System.out.println(i);
+               // last biggest diff
+                int diff = maxOfFour(i) - minOfFour(i);
+                if (diff > maxDiff) {
+                    maxDiff = diff;
+                    maxDiffNum = i;
+                }
+
+
+            }
+        }
+
+
+        System.out.println(
+                "The number with the biggest difference between the largest and smallest digits is: " + maxDiffNum
+        );
+
+
+
+    }
+
+    private static int inputNumber() {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        if (num < 1000 || num > 9999) {
+            System.out.println("Invalid input. Please enter a 4-digit number.");
+            return inputNumber();
+        }
+        return num;
+    }
+
+    public static boolean run(int userNumber) {
+
+
+        int thousands = (userNumber / 1000) % 10;
+        int hundreds = (userNumber / 100) % 10;
+        int tens = (userNumber / 10) % 10;
+        int units = userNumber % 10;
+
+        int evenDigitSum = sumOfEvenDigits(thousands, hundreds, tens, units);
+        int firstLastSum = thousands + units;
+
+        // difference between thousands and hundreds is more than 6 + difference between thousands and tens is more than 6;
+        int diffTH = thousands - hundreds;
+        int diffTT = thousands - tens;
+
+        boolean checkDiffTH = turnPositive(diffTH) > 6;
+        boolean checkDiffTT = turnPositive(diffTT) > 6;
+
+
+       return (evenDigitSum <= 8) && (firstLastSum <= 3) && checkDiffTH && checkDiffTT;
+    }
+
+    private static int turnPositive(int num) {
+        return num < 0 ? -num : num;
+    }
+
+    private static int sumOfEvenDigits(int... digits) {
+        int sum = 0;
+        for (int digit : digits) {
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+
+    private static int maxOfFour(int userNumber) {
+        int a = (userNumber / 1000) % 10;
+        int b = (userNumber / 100) % 10;
+        int c = (userNumber / 10) % 10;
+        int d = userNumber % 10;
+        return Math.max(Math.max(a, b), Math.max(c, d));
+    }
+
+    private static int minOfFour(int userNumber) {
+       int a = (userNumber / 1000) % 10;
+        int b = (userNumber / 100) % 10;
+        int c = (userNumber / 10) % 10;
+        int d = userNumber % 10;
+        return Math.min(Math.min(a, b), Math.min(c, d));
+    }
+}
--- src/livework/Main.java
+++ src/livework/Main.java
@@ -1,4 +1,26 @@
 package livework;
 
+import java.util.*;
+
 public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+// The variable "num" holds an integer user input
+// Write a conditional statement starting on line 9 that does the following:
+//
+//-  If num is positive, print "__ is positive"
+//-  if num is negative, print "__ is negative"
+        int num;
+        System.out.println("Enter a number");
+        num = in.nextInt();
+        if (num < 0)
+            System.out.println(num + "is negative");
+        else {
+            System.out.println(num + "is positive");
+        }
+
+    }
+
 }
